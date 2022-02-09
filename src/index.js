13757ee8981e91f756191aae5303c0f0ad4710d6
kilokid <arlomov007@gmail.com>
2022-02-09 19:56:02 +0300

    feat: added markup of the application and its styles and photos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import './scss/app.scss';
 
 import App from './App';
 
@@ -9,5 +9,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
