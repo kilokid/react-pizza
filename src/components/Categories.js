b3a8502da9bbae1bd5c8c0fc8a4ca573d6823460
kilokid <arlomov007@gmail.com>
2022-02-13 10:45:57 +0300

    feat: menu and sorting components created, switching and sorting logic added

---
+++ src/components/Categories.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const Categories = ({ items }) => {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const onSelectItem = (i) => {
+    setActiveItem(i);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>
+          Все
+        </li>
+        {items?.map((item, i) => (
+          <li
+            className={activeItem === i ? 'active' : ''}
+            onClick={() => onSelectItem(i)}
+            key={`${item}_${i}`}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
