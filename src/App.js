dfa64ea7066a2354ec85f34d492cff74db7ebaa8
kilokid <arlomov007@gmail.com>
2022-02-17 12:30:40 +0300

    feat: redux logic moved to components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import axios from 'axios';
-
-import { useDispatch } from 'react-redux';
-
-import { setPizzas } from './redux/action/pizzas';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
